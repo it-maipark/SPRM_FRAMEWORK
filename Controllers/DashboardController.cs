7ae7a8f4ad295e2ed6b3da57e89bdac43687b009
izviopm <prijayaizvio@gmail.com>
2025-03-30 17:37:23 +0700

    Layout

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SPRM_FRAMEWORK.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
