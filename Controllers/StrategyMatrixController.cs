f67445f2eca482efbd0a4325ef293d6e32954e09
izviopm <prijayaizvio@gmail.com>
2025-04-15 14:47:12 +0700

    Add Strategy

---
+++ Controllers/StrategyMatrixController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SPRM_FRAMEWORK.Controllers
+{
+    public class StrategyMatrixController : Controller
+    {
+        public IActionResult Profitability()
+        {
+            return View();
+        }
+
+        public IActionResult Sustainability()
+        {
+            return View();
+        }
+
+        public IActionResult SDGs()
+        {
+            return View();
+        }
+
+        public IActionResult HCR()
+        {
+            return View();
+        }
+
+        public IActionResult Improvement()
+        {
+            return View();
+        }
+    }
+}
