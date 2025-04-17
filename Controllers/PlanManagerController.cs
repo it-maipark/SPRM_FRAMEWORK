857cd3eee908d1468c4f0a16d26d0ce79fa441dd
izviopm <prijayaizvio@gmail.com>
2025-04-17 16:52:28 +0700

    Update Layout
    Add Kanban layout temp, calendar and other page blank page

---
+++ Controllers/PlanManagerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SPRM_FRAMEWORK.Controllers
+{
+    public class PlanManagerController : Controller
+    {
+        public IActionResult Grid()
+        {
+            return View();
+        }
+        public IActionResult Gantt()
+        {
+            return View();
+        }
+        public IActionResult Kanban()
+        {
+            return View();
+        }
+        public IActionResult Calendar()
+        {
+            return View();
+        }
+        public IActionResult Graph()
+        {
+            return View();
+        }
+    }
+}
